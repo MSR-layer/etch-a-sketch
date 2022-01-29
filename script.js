fb1ede48ec6460acf5a81a3eb16bd507cf9b9247
MSR-layer <marah44413@gmail.com>
2022-01-29 16:18:32 +0300

    added rainbow option

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 /**
  * TO-DO:
- * add clear button []
+ * add clear button [x]
  * add slider for number of cells per side []
- * add rainbow option []
+ * add rainbow option [x]
  */
 
 
@@ -25,22 +25,50 @@ function initialize()
     }
 }
 
-function onHover(){
+function mono(){
     this.style.backgroundColor = 'black';
 }
 
 function clear(){
-    console.log('clea');
     divList.forEach(div => div.style.backgroundColor = 'white');
+}
+
+function randomColorGenerator(){
+    let maxVal = 0xFFFFFF;
+    let randomNumber = Math.random() * maxVal;
+    randomNumber = Math.floor(randomNumber);
+    randomNumber = randomNumber.toString(16);
+    let randomColor = randomNumber.padStart(6,0);
+    console.log(randomColor);
+    return `#${randomColor}`;
+}
+
+function rainbow(){
+    this.style.backgroundColor = `${randomColorGenerator()}`;
+}
+
+function switchPalette(){
+    if(this.id === 'rainbow'){
+        divList.forEach(div => div.removeEventListener('mouseover', mono));
+        divList.forEach(div => div.addEventListener('mouseover', rainbow));
+    }else{
+        divList.forEach(div => div.removeEventListener('mouseover', rainbow));
+        divList.forEach(div => div.addEventListener('mouseover', mono));
+    }
 }
 
 initialize();
 
 const clearButton = document.getElementById('clear');
+const rainbowButton = document.getElementById('rainbow');
+const monoButton = document.getElementById('monochrome');
 
-clearButton.addEventListener('click', clear);
+
 
 const divList = Array.from(document.querySelectorAll('.grid-cell'));
 
-divList.forEach(div => div.addEventListener('mouseover', onHover));
+divList.forEach(div => div.addEventListener('mouseover', mono));
 
+clearButton.addEventListener('click', clear);
+rainbowButton.addEventListener('click', switchPalette);
+monoButton.addEventListener('click', switchPalette);
